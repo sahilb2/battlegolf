f7e3ad9f6bf02479de2f370e9143838c2c571466
Ben Morrison <newben.hd@gmail.com>
2016-11-17 22:11:02 -0600

    basic structures for board is done

--- src/Game.java
+++ src/Game.java
@@ -22,6 +22,38 @@ public class Game {
 			5	Destroyer	2
 		 * Ships cannot overlap or go out of bounds
 		 */
+		int[] ships = new int[]{5, 4, 3, 3, 2};
+		int counter = 0;
+		while(counter < 5){
+			int x = (int)Math.random()*8;
+			int y = (int)Math.random()*8;
+			//true == vertical, false == horizontal
+			boolean orien = (int)Math.random()*2 == 1;
+			if(board[x][y]==null){
+				if(orien){
+					if(ships[counter]+x < 8){
+						boolean passed = true;
+						for(int i = x; i < x + ships[counter]; i++){
+							if(boards[i][y] != null)
+								passed = false;
+						}
+						if(passed)
+							//create object	
+					}
+				}
+				else{
+					if(ships[counter]+y < 8){
+						boolean passed = true;
+						for(int i = y; i < y + ships[counter]; i++){
+							if(boards[x][i] != null)
+								passed = false;
+						}
+						if(passed)
+							//create object
+					}
+				}
+			}
+		}
 	}
 	
 	public Ship checkHit(int x, int y){
