9ce25d92ec5e80e2c5ee7d69c5e93d6de520cd7b
sahilb2 <sahilbhatt@hotmail.com>
2016-11-20 22:14:55 -0600

    Finished final draft with comments

--- src/Board.java
+++ src/Board.java
@@ -1,23 +1,25 @@
 public class Board{
-    
+    // Protected instance variables in order to gain accessibility throughout this package
 	protected int numholes = 17;
 	protected int[] shipSize = new int[]{5,4,3,3,2};
     protected int[][] boards = new int[8][8];
     
     public Board(){
+    	// Constructor calls createBoard() method
     	createBoard();
     }
     
     public boolean hit(int x, int y){
-		if(boards[x][y] == 1){
-		    boards[x][y] = 0;
-		    numholes--;
-		    return true;
+		if(boards[x][y] == 1){ // if there is a ship,
+		    boards[x][y] = 0; // change it to no ship
+		    numholes--; // decrease number of ship holes by 1
+		    return true; // return true for hit
 		}
-		return false;
+		return false; // else, return false for hit
 	}
     
     public void printBoard(){
+    	// Not used in main, used for debugging purposes only!
     	for (int i = 0; i < boards.length; i++){
     		for (int j = 0; j < boards[i].length; j++){
     			System.out.print(boards[i][j]);
@@ -27,27 +29,29 @@ public class Board{
     }
     
     public int[][] createBoard(){
-		int counter = 0, x = 0, y = 0;
-		while(counter < 5){
-		    x = (int)(Math.random()*8);
-		    y = (int)(Math.random()*8);
-		    boolean dir = (int)(Math.random()*2) == 0;
-		    if(dir){
-				if(y + shipSize[counter] <= 8){
+    	// Most tricky part of the project
+		int counter = 0, x = 0, y = 0; // Instantiating variable
+		while(counter < 5){ // Until 5 ships are placed,
+		    x = (int)(Math.random()*8); // x-coordinate randomly selected
+		    y = (int)(Math.random()*8); // y-coordinate randomly selected
+		    boolean dir = (int)(Math.random()*2) == 1; // direction randomly selected
+		    	// Used boolean since it can only be horizontal or vertical
+		    if(dir){ // If random dir is vertical,
+				if(y + shipSize[counter] <= 8){ // Checks if it fits inside the box
 				    boolean empty = true;
-				    for(int i = y; i < y + shipSize[counter]; i++){
+				    for(int i = y; i < y + shipSize[counter]; i++){ // Checks if all those spaces are empty
 				    	if(boards[x][i] == 1)
 				    		empty = false;
 				    }
-				    if(empty){
+				    if(empty){ // If spaces are empty,
 				    	for(int i=y; i < y + shipSize[counter]; i++){
-				    		boards[x][i] = 1;
+				    		boards[x][i] = 1; // Places ships (1) inside the box
 				    	}
-				    	counter++;
+				    	counter++; // Ship has been added!
 				    }
 				}
 		    }
-		    else{
+		    else{ // Goes through the same exact algorithm (with different orientation of coordinates) if the random dir is horizontal
 				if(x + shipSize[counter] <= 8){
 				    boolean empty = true;
 				    for(int i = x; i < x + shipSize[counter]; i++){
@@ -63,6 +67,6 @@ public class Board{
 				}
 		    }
 		}
-		return boards;
+		return boards; // Returns completed board with 0 meaning no ships and 1 meaning ship spot
     }
 } 
--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // Used to import Scanner to read user's inputs
 
 public class Game{
     
 	public static void main(String[] args){
-	
-		System.out.println("Welcome to BattleGolf!");
-		Scanner scan = new Scanner(System.in);
-		int coorx = -1,coory = -1;
-		int counter = 0;
-		Board board = new Board();
-		board.printBoard();
+
+		System.out.println("Welcome to BattleGolf!"); // Intro to BattleGolf!
+		Scanner scan = new Scanner(System.in); // Used to read user's input
+		int coorx = -1,coory = -1; // initializing board coordinates to -1 (invalid number)
+		int counter = 0; // counter that counts number of tries
+		Board board = new Board(); // Calls Board class to create the board
 		System.out.println("The board is randomly set up! Lets begin the game!");
-		while (board.numholes>0) // while game not done...
+		while (board.numholes>0) // While more than 0 ships are alive. (17 holes in this game).
 		{	
 			System.out.println("Pick your lucky point! For example, A-3 or B-4 (Ranging from A through H for x coordinates and 1 to 8 for y-coordinates)");
 			boolean isValid = false;
-			while(!isValid) // checks validity of 
+			while(!isValid) // checks validity of inputs
 			{
 				String input = scan.nextLine();
 				if (input.length() == 3){
+					/*
+					 * The next lines of code directs inputs to coordinates in our 2d dimension.
+					 * It works for both uppercase and lowercase characters.
+					 * For example, A-1 => (0,0), A-2 => (0,1), and so on...
+					 */
 					if (input.charAt(0) == 'a' || input.charAt(0) == 'A') coorx = 0;
 					if (input.charAt(0) == 'b' || input.charAt(0) == 'B') coorx = 1;
 					if (input.charAt(0) == 'c' || input.charAt(0) == 'C') coorx = 2;
@@ -37,23 +41,25 @@ public class Game{
 					if (input.charAt(2) == '8') coory = 7;					
 				}
 				if (coorx != -1 && coory != -1){
-					isValid = true;
+					isValid = true; // Gets out of the isValid loop if the coordinates are valid
 				}
-				else{
+				else{ // Asks user to input correctly formatted coordinates until they input them
 					System.out.println("Please insert the coordinate in correct format. For example, A-3 or B-4 (Ranging from A through H for x coordinates and 1 to 8 for y-coordinates)");
 				}
 			}
+			// Calls Board class to determine if hit or miss
 			if (board.hit(coorx, coory)){
 				System.out.println("HIT!!!");
 			}
 			else{
 				System.out.println("Miss :( Try Again!");
 			}
+			// Resets coordinates to invalid value and count of tries increases by 1
 			coorx = -1;
 			coory = -1;
 			counter++;
-			board.printBoard();
 		}
+		// Game over! Prints the number of tries it took to finish the game!
 		System.out.println("All ships have been sunk! Total number of shots taken: " + counter);
     }
 }
